01ac2e7b1005e21a79557141beec2ec349c46934
Kipleting <kipletingedwin4@gmail.com>
2022-09-13 09:54:13 +0300

    remove unwanted code in journal.js

--- src/components/journal.js
+++ src/components/journal.js
@@ -14,7 +14,6 @@ function Journal({town_name,country_name,date,description,handleDeleteClick,id})
           <h4><span>Town</span>:{town_name}</h4>
           <h4><span>Year</span>:{date}</h4>
           <p><span>Description</span>:{description}</p>
-             {/* <button id='button2'>Edit</button> */}
              <button id='button2' onClick={()=>handleDeleteClick(id)} >Delete</button>
         </div>
     </div>
